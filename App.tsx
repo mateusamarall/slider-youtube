53cb5148f243bf9ab465070c896ea371956d76a1
mateusamarall <mateus.amaral018@gmail.com>
2021-01-12 18:06:09 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React,{useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Slider from '@react-native-community/slider';
+
 
 export default function App() {
+  const [valor, setValor] = useState<number>();
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{valor}</Text>
+     <Slider
+     style={{width:200, height:40}}
+     minimumValue={0}
+     step={5}
+     maximumValue={100}
+     minimumTrackTintColor="#000"
+     maximumTrackTintColor="#3333ff"
+     thumbImage={{uri:('https://i.pinimg.com/originals/d9/d7/79/d9d7796f0b3834832ffd44c177fb4370.png')}}
+     onValueChange={(val)=> setValor(val)}
+     />
     </View>
   );
 }
